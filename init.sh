81cffe2ecb29d8cf11e22f60c93f137c4226b01f
danlin <jdlsx21499@gamil.com>
2022-09-01 15:11:33 -0400

    add comments and stdout to terminal

--- init.sh
+++ init.sh
@@ -1,16 +1,19 @@
 SNIS=$(pwd)
 
 # initialize submodule
+echo "initialize submodule:"
 git submodule update --init --recursive --remote
 git submodule sync
 cd SSD_Sim && git pull https://github.com/DanlinJia/SSD_Sim.git main
 
 # compile MQSim and copy it to workspace
-cd SSD_Sim/MQSim && make
+echo "compile MQSim and copy it to workspace:"
+cd MQSim && make
 cd $SNIS
 cp SSD_Sim/MQSim/MQSim ssd_work_space/
 chmod +x ssd_work_space/MQSim
 
+echo "copy necessary files"
 # copy MQSim configuration files
 cp SSD_Sim/MQSim/ssdconfig.test.xml ssd_work_space/
 cp SSD_Sim/MQSim/workload-trace.xml ssd_work_space/
